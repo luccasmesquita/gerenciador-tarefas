ba1d052105c5e950444f5cd37f17d6c0b5194025
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-05-20 10:47:53 +0100

    primeiro commit - menu inicial

---
+++ gerenciador.py
@@ -0,0 +1,15 @@
+while True: 
+    print('\nMenu do Gerenciador de Lista de tarefas: ')
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefa")
+    print("3. Atualizar tarefa")
+    print("4. Compretar tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    escolha = input("\nDigite a sua escolhe: ")
+
+    if escolha == "6":
+        break
+
+print("\nPrograma finalizado")
